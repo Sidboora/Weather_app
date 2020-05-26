b335da20226d0ba62bcd7cfe9a8a02e8d82929b1
Sidboora <sidboora1998@gmail.com>
2020-05-26 12:42:14 +0530

    first commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=b09a84dc6c2ad15be1e70c69e2011186&query='+ latitude + ',' + longitude
+
+    request({ url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)}
+        // else if (response.body.current==0) {
+        //     callback('Unable to find location', undefined)
+        // } 
+        else {
+            callback(undefined,  ' It is currently ' + response.body.current.temperature+ ' degress out. ')
+        }
+    })
+}
+
+module.exports = forecast
