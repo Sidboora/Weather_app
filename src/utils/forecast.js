349448bd0daf1f72a05240151f4a47b32d727470
Sidboora <sidboora1998@gmail.com>
2020-05-26 17:20:54 +0530

    add about text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         //     callback('Unable to find location', undefined)
         // } 
         else {
-            callback(undefined,  ' It is currently ' + response.body.current.temperature+ ' degrees out. ')
+            callback(undefined,  ' It is currently ' + response.body.current.temperature+ ' degrees out. There are '+response.body.current.precip'% chances of rain')
         }
     })
 }
